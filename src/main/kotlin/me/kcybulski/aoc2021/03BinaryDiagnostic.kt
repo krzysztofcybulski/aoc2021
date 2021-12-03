bdb93e17d1e2666d7fd4d84ae4f999f0e242372a
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-12-03 15:02:54 +0100

    Day 3 | Binary Diagnostic - part 2

--- src/main/kotlin/me/kcybulski/aoc2021/03BinaryDiagnostic.kt
+++ src/main/kotlin/me/kcybulski/aoc2021/03BinaryDiagnostic.kt
@@ -3,23 +3,53 @@ package me.kcybulski.aoc2021
 import me.kcybulski.aoc2021.utils.lines
 import me.kcybulski.aoc2021.utils.print
 
+//@formatter:off
 fun main() {
     val lines = lines("03BinaryDiagnostic")
 
-    val mostCommon = (0 until lines[0].length)
-        .map { index ->
-            lines
-                .map { it[index] }
-                .groupingBy { it }
-                .eachCount()
-                .maxByOrNull { it.value }
-                ?.key
-        }
-        .joinToString("")
+    (
+            lines.mostCommon().fromBinary()
+                    *
+            lines.leastCommon().fromBinary()
+    ).print()
 
-    (mostCommon.fromBinary() * mostCommon.inverse().fromBinary()).print()
+    (
+            find(lines, List<String>::mostCommon)[0].fromBinary()
+                    *
+            find(lines, List<String>::leastCommon)[0].fromBinary()
+    ).print()
+}
+//@formatter:on
+
+private fun find(raport: List<String>, selector: (List<String>) -> String, index: Int = 0): List<String> =
+    if (raport.size == 1) raport else {
+        val mostCommon = selector(raport)
+        find(raport.filter { it[index] == mostCommon[index] }, selector, index + 1)
+    }
+
+private fun List<String>.mostCommon() = common { entries ->
+    entries
+        .sortedByDescending { it.key }
+        .maxByOrNull { it.value }
+        ?.key
 }
 
-private fun String.inverse() = map { if (it == '0') '1' else '0' }.joinToString("")
+private fun List<String>.leastCommon() = common { entries ->
+    entries
+        .sortedBy { it.key }
+        .minByOrNull { it.value }
+        ?.key
+}
+
+private fun List<String>.common(handler: (List<Map.Entry<Char, Int>>) -> Char?) = (0 until first().length)
+    .map { index ->
+        map { it[index] }
+            .groupingBy { it }
+            .eachCount()
+            .entries
+            .sortedBy() { it.key }
+            .let(handler)
+    }
+    .joinToString("")
 
 private fun String.fromBinary() = toInt(2)
