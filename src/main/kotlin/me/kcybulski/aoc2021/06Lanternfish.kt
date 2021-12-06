4a0064db60b11685f639a6739e9cd6cb2f121e4c
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-12-06 22:25:54 +0100

    Day 6 | Lanterfish - part 2

--- src/main/kotlin/me/kcybulski/aoc2021/06Lanternfish.kt
+++ src/main/kotlin/me/kcybulski/aoc2021/06Lanternfish.kt
@@ -1,4 +1,5 @@
 package me.kcybulski.aoc2021
+
 import me.kcybulski.aoc2021.utils.lines
 import me.kcybulski.aoc2021.utils.print
 
@@ -8,22 +9,35 @@ fun main() {
         .split(",")
         .map(String::toInt)
         .map(::Lanternfish)
-        .let { days(it, 80) }
+        .let { days(it.groupingBy(Lanternfish::age).eachCount()
+            .mapValues { it.value.toLong() }
+            .mapKeys { Lanternfish(it.key) }, 256) }
         .print()
 }
 
-private fun days(fishes: List<Lanternfish>, days: Int): Int {
-    if(days == 0) {
-        return fishes.size
+private tailrec fun days(fishes: Map<Lanternfish, Long>, days: Int): Long {
+    if (days == 0) {
+        println(fishes)
+        return fishes.values.sum()
     }
-    return days(fishes.flatMap { it.next() }, days - 1)
+    val fishesToAdd = fishes.map { (k, v) -> k.next().mapValues { (a, b) -> b * v } }
+        .reduce { acc, map -> acc.add(map) }
+        .filterValues { it != 0L }
+    return days(fishesToAdd, days - 1)
 }
 
-data class Lanternfish(val age: Int) {
+@JvmInline
+value class Lanternfish(val age: Int) {
 
-    fun next(): List<Lanternfish> = when(age) {
-        0 -> listOf(Lanternfish(6), Lanternfish(8))
-        else -> listOf(Lanternfish(age - 1))
+    fun next(): Map<Lanternfish, Int> = when (age) {
+        0 -> mapOf(Lanternfish(6) to 1, Lanternfish(8) to 1)
+        else -> mapOf(Lanternfish(age - 1) to 1)
     }
 
 }
+
+private fun Map<Lanternfish, Long>.add(other: Map<Lanternfish, Long>) =
+    (0..9).associate { i ->
+        val lanternfish = Lanternfish(i)
+        lanternfish to ((this[lanternfish] ?: 0) + (other[lanternfish] ?: 0))
+    }
