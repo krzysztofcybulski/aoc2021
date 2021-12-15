bde26353ca82973afa3fc5ac715c99da21c1c4ad
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2021-12-15 05:58:26 +0100

    Day 14 | Extended Polymerization - part 2

--- src/main/kotlin/me/kcybulski/aoc2021/14ExtendedPolymerization.kt
+++ src/main/kotlin/me/kcybulski/aoc2021/14ExtendedPolymerization.kt
@@ -1,9 +1,7 @@
 package me.kcybulski.aoc2021
+
 import me.kcybulski.aoc2021.utils.lines
 import me.kcybulski.aoc2021.utils.print
-
-class ExtendedPolymerization() {
-}
 
 fun main() {
     val lines = lines("14ExtendedPolymerization")
@@ -12,14 +10,36 @@ fun main() {
         .map { it.split(" -> ") }
         .associate { it[0] to it[1] }
 
-    val elements = (0..9).fold(template) { temp, _ -> temp.insert(rules) }
-        .groupingBy { it }
-        .eachCount()
-        .map { it.value }
+
+    val d20 = rules
+        .mapValues { (k, _) -> k.insert(rules, 21) }
+        .mapValues { (_, v) -> v.countLetters() }
+
+    val elements = template.insert(rules, 17)
+        .windowed(2, 1)
+        .mapNotNull { d20[it]?.subtract(it.last()) }
+        .reduce(Map<Char, Long>::add)
+        .add(mapOf(template.last() to 1L))
+        .map(Map.Entry<Char, Long>::value)
 
     ((elements.maxOrNull() ?: 0) - (elements.minOrNull() ?: 0)).print()
 }
 
-private fun String.insert(rules: Map<String, String>) =
+private fun Map<Char, Long>.add(second: Map<Char, Long>) =
+    mapValues { (key, value) -> value + (second[key] ?: 0L) } +
+            second.mapValues { (key, value) -> value + (this[key] ?: 0L) }
+
+private fun Map<Char, Long>.subtract(l: Char) =
+    mapValues { (key, value) -> if (key == l) value - 1 else value }
+
+private fun String.insert(rules: Map<String, String>, depth: Int) =
+    (0..depth).fold(this) { e, _ -> e.insert(rules) }
+
+private fun String.insert(rules: Map<String, String>): String =
     windowed(2, 1)
         .joinToString("") { it[0] + (rules[it] ?: "") } + last()
+
+private fun String.countLetters() =
+    groupingBy { it }
+        .eachCount()
+        .mapValues { it.value.toLong() }
